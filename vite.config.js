a33be5e16a580d6563cacb8870ab188390678a7e
Eray Dura <eraydura@hotmail.com>
2023-09-27 18:22:43 +0300

    second

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.gltf'],
+  assetsInclude: ['**/*.gltf','**/*.bin','**/*.glb'],
   plugins: [react()]
 })
 
